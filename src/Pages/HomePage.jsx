248745fd6b9e63eb21fed41acdbc799e3a6afcba
naim-haider <naimhaider76@gmail.com>
2024-03-08 01:08:27 +0530

    first commit

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  Tabs,
+  Text,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../Components/Authentication/Login";
+import Signup from "../Components/Authentication/Signup";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) {
+      navigate("/chats");
+    }
+  }, [navigate]);
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        color={"black"}
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          SpoutChat
+        </Text>
+      </Box>
+      <Box
+        bg={"white"}
+        w={"100%"}
+        p={4}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
